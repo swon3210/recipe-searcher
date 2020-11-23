116a48b8414da46316d6bdfe42710b18d8f9e5b8
swon3210 <swon3210@gmail.com>
2020-11-24 01:01:34 +0900

    change hash routing to pushstate
    - media query adjusted

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,15 +9,51 @@ sidebarCloseBtn.addEventListener('click', toggleSidebar);
 
 // Sidebar
 const sidebar = document.querySelector('.sidebar');
+const sidebarItems = document.querySelector('.sidebar-items');
 let sidebarX = -260;
 
 // SEARCH 1 INPUTS
-const search1Inputs = document.querySelectorAll('#search-1 .text-input');
-console.log(search1Inputs);
+const search1Inputs = document.querySelectorAll('#search .text-input');
+const nextBtn = document.querySelector('#search .next-btn');
+const goBackBtn = document.querySelector('#search-detail .go-back-btn');
+const searchStartBtn = document.querySelector('#search-detail .next-btn');
+
+nextBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const SEARCH_DETAIL_PAGE = 'SEARCH-DETAIL'
+  history.pushState(null, null, `/${SEARCH_DETAIL_PAGE.toLowerCase()}`)
+  headerText.innerText = SEARCH_DETAIL_PAGE;
+  pageTransition(SEARCH_DETAIL_PAGE.toLowerCase());
+})
+
+
+goBackBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const SEARCH_DETAIL_PAGE = 'SEARCH'
+  history.pushState(null, null, `/${SEARCH_DETAIL_PAGE.toLowerCase()}`)
+  headerText.innerText = SEARCH_DETAIL_PAGE;
+  pageTransition(SEARCH_DETAIL_PAGE.toLowerCase());
+})
+
+searchStartBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const SEARCH_DETAIL_PAGE = 'RECIPES'
+  history.pushState(null, null, `/${SEARCH_DETAIL_PAGE.toLowerCase()}`)
+  headerText.innerText = SEARCH_DETAIL_PAGE;
+  pageTransition(SEARCH_DETAIL_PAGE.toLowerCase());
+})
 
 // SEARCH 2 INPUTS
-const timeRangeInput = document.querySelector('#search-2 .time-range-input');
+const timeRangeInput = document.querySelector('#search-detail .time-range-input');
+const timeRangeInputValue = document.querySelector('#search-detail .time-range-input-value');
+const timeRangeTrackbar = document.querySelector('#search-detail .time-range-input-trackbar');
 const selectedIngredients = []
+
+timeRangeInput.addEventListener('input', (event) => {
+  const progress = event.target.value;
+  timeRangeInputValue.innerText = progress + ' min'
+  timeRangeTrackbar.style.transform = `translateX(-${100 - progress * (2 / 3)}%)`;
+})
 
 // Page Transition
 // const search1Page = document.querySelector('#search-1');
@@ -30,11 +66,28 @@ const selectedIngredients = []
 const pages = document.getElementsByClassName('page');
 
 // Routing
-window.onhashchange = () => {
-  const page = window.location.hash.substr(1);
-  headerText.innerText = page;
-  pageTransition(page)
-}
+// window.onhashchange = () => {
+//   const page = window.location.hash.substr(1);
+//   headerText.innerText = page;
+//   if (page === '') {
+//     pageTransition('recipes')
+//   } else {
+//     pageTransition(page)
+//   }
+// }
+
+sidebarItems.addEventListener('click', (event) => {
+  event.preventDefault();
+  let page;
+  if (event.currentTarget.innerText !== event.target.innerText) {
+    page = event.target.innerText
+    history.pushState(null, null, `/${page.toLowerCase()}`)
+    headerText.innerText = page;
+    pageTransition(page.toLowerCase());
+  }
+})
+
+// 리다이렉팅 문제...
 
 // Page Transition
 function pageTransition(pageName) {
@@ -49,7 +102,6 @@ function pageTransition(pageName) {
 
 // Sidebar Function
 function toggleSidebar() {
-  console.log(sidebarX)
   if (sidebarX >= 0) {
     closeSidebar()
   } else {
